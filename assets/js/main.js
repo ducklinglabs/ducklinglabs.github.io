07bb422edef1a865a9641b5ebfeeb746fcf14764
Badami <srahulbadami@gmail.com>
2022-04-01 14:56:08 +0530

    Updated Images

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function () {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-white.svg");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-white.svg");
         }
     });
 
